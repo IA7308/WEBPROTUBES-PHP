51a8a8a9c9e84787dc64d1d6a89b36b2ab4476c8
andryfall <andryfall@student.telkomuniversity.ac.id>
2024-11-04 16:47:12 +0700

    Update ArtikelTest.php

--- laravel/tests/Feature/ArtikelTest.php
+++ laravel/tests/Feature/ArtikelTest.php
@@ -13,7 +13,7 @@ class ArtikelTest extends TestCase
     /** @test */
     public function it_displays_the_correct_count_in_the_view()
     {
-        Artikel::factory()->count(5)->create();
+        artikel::factory()->count(5)->create();
 
         $response = $this->get(route('artikel.index'));
 
