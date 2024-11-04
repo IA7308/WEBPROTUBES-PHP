c43422d3765d429123c351aabbcc5aa42eb77002
andryfall <andryfall@student.telkomuniversity.ac.id>
2024-11-04 16:48:53 +0700

    Update ArtikelTest.php

--- laravel/tests/Feature/ArtikelTest.php
+++ laravel/tests/Feature/ArtikelTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use App\Models\Artikel;
+use App\Models\artikel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ArtikelTest extends TestCase
